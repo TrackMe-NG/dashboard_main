fc078739ebe8a8dc509f04d63666c7b07bca7461
Mozibix <opuzmoses@gmail.com>
2023-10-20 16:06:49 +0100

    chores: updated  DashBoardLayout component

---
+++ src/layouts/DashboardLayout.jsx
@@ -0,0 +1,37 @@
+import PropTypes from "prop-types";
+import { Header } from "../(components)/Header";
+import { SideNav } from "../(components)/SideNav";
+
+const DashboardLayout = ({ children }) => {
+  return (
+    <>
+      <div className="dashboard_main">
+        <div className="dashboard_main_inner">
+          <div className="dashboard_main_left">
+            {/* SIDENAV SECTION */}
+            <div className="sidenav_main">
+              <SideNav />
+            </div>
+          </div>
+          <div className="dashboard_main_right">
+            <div className="dashboard_main_right_inner">
+              {/* HEADER SECTION */}
+              <div>
+                <Header />
+              </div>
+
+              {/* MAIN CONTENTS */}
+              <div>{children}</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default DashboardLayout;
+
+DashboardLayout.propTypes = {
+  children: PropTypes.node,
+};
