c9874264a539f53e45f6ff5a1a6cd012ea02e8ce
Mozibix <opuzmoses@gmail.com>
2023-10-20 23:42:18 +0100

    chores: updated Dashboard component

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -1,11 +1,16 @@
 import DashboardLayout from "./layouts/DashboardLayout";
 import { Summary } from "./(components)/Summary";
+import { Subscription } from "./(components)/Subscription";
 
 const Dashboard = () => {
   return (
     <>
       <DashboardLayout>
+        {/* SUMMARY SECTION */}
         <Summary />
+
+        {/* SUBSCRIPTION SECTION */}
+        <Subscription />
       </DashboardLayout>
     </>
   );
