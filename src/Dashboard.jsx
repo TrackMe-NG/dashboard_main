76d260e288a745cc4acc0a89929a99bab1e68e4a
Mozibix <opuzmoses@gmail.com>
2023-10-20 16:06:07 +0100

    chores: updated dashboard component

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -1,32 +1,12 @@
-import { Header } from "./(components)/Header";
-import { SideNav } from "./(components)/SideNav";
+import DashboardLayout from "./layouts/DashboardLayout";
 import { Summary } from "./(components)/Summary";
 
 const Dashboard = () => {
   return (
     <>
-      <div className="dashboard_main">
-        <div className="dashboard_main_inner">
-          <div className="dashboard_main_left">
-            {/* SIDENAV SECTION */}
-            <div className="sidenav_main">
-              <SideNav />
-            </div>
-          </div>
-          <div className="dashboard_main_right">
-            <div className="dashboard_main_right_inner">
-              {/* HEADER SECTION */}
-              <div>
-                <Header />
-              </div>
-              {/* SUMMARY SECTION */}
-              <div>
-                <Summary />
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <DashboardLayout>
+        <Summary />
+      </DashboardLayout>
     </>
   );
 };
