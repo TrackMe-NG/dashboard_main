2095e1253092ecffff73c6b1aa5303a1896b7ed1
Mozibix <opuzmoses@gmail.com>
2023-10-21 12:56:09 +0100

    chores: updated settings page component

---
+++ src/pages/Settings.jsx
@@ -0,0 +1,11 @@
+import DashboardLayout from "../layouts/DashboardLayout";
+
+export const Settings = () => {
+  return (
+    <>
+      <DashboardLayout>
+        <p>hello world</p>
+      </DashboardLayout>
+    </>
+  );
+};
