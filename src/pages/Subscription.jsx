0d32f598e48d166e6690f8b112b324e738d7f6ec
Mozibix <opuzmoses@gmail.com>
2023-10-21 16:00:44 +0100

    chores: updated subscription page component

--- src/pages/Subscription.jsx
+++ src/pages/Subscription.jsx
@@ -1,10 +1,28 @@
+import { Revenue } from "../(components)/Revenue";
+import { SubscriptionComps } from "../(components)/SubscriptionComps";
+import { TopSeller } from "../(components)/TopSeller";
 import DashboardLayout from "../layouts/DashboardLayout";
 
 export const Subscription = () => {
   return (
     <>
       <DashboardLayout>
-        <p>hello world</p>
+        <div className="subscription_page">
+          {/* SUBSCRIPTION COMPS SEC */}
+          <div className="subscription_page_left">
+            <SubscriptionComps />
+          </div>
+
+          <div className="subscription_page_right">
+            <div>
+              <TopSeller />
+            </div>
+
+            <div>
+              <Revenue />
+            </div>
+          </div>
+        </div>
       </DashboardLayout>
     </>
   );
