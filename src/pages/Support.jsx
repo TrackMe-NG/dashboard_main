45f97242913ce067d67175fbd0d0e6d464a67f89
Mozibix <opuzmoses@gmail.com>
2023-10-21 12:56:39 +0100

    chores: updated support page component

---
+++ src/pages/Support.jsx
@@ -0,0 +1,11 @@
+import DashboardLayout from "../layouts/DashboardLayout";
+
+export const Support = () => {
+  return (
+    <>
+      <DashboardLayout>
+        <p>hello world</p>
+      </DashboardLayout>
+    </>
+  );
+};
