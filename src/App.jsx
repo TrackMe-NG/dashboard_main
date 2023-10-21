adbeb998f9b1f273b96b047293648e38bb889a85
Mozibix <opuzmoses@gmail.com>
2023-10-21 12:50:19 +0100

    chores: updated app routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Dashboard from "./Dashboard";
+import { Product } from "./pages/Product";
+import { Settings } from "./pages/Settings";
+import { Support } from "./pages/Support";
+import { Customer } from "./pages/Customer";
+import { Subscription } from "./pages/Subscription";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Dashboard />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="/customer" element={<Customer />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="/settings" element={<Settings />} />
+        <Route path="/subscription" element={<Subscription />} />
+        <Route path="/support" element={<Support />} />
       </Routes>
     </Router>
   );
