84d1dbb6224094bbf6623b0b33f34a8788f48a29
Mozibix <opuzmoses@gmail.com>
2023-10-21 16:06:38 +0100

    chores: updated topseller component

--- src/(components)/TopSeller.jsx
+++ src/(components)/TopSeller.jsx
@@ -1,3 +1,11 @@
 export const TopSeller = () => {
-  return <div>TopSeller</div>;
+  return (
+    <>
+      <div className="topseller">
+        <div className="topseller_inner">
+          <h6>top seller</h6>
+        </div>
+      </div>
+    </>
+  );
 };
