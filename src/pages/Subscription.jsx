1bd921851b15fb48916061c800cf87f451d5bf15
Mozibix <opuzmoses@gmail.com>
2023-10-21 12:56:24 +0100

    chores: updated subscription page component

---
+++ src/pages/Subscription.jsx
@@ -0,0 +1,11 @@
+import DashboardLayout from "../layouts/DashboardLayout";
+
+export const Subscription = () => {
+  return (
+    <>
+      <DashboardLayout>
+        <p>hello world</p>
+      </DashboardLayout>
+    </>
+  );
+};
