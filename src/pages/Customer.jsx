15faa592e71f89e2defa4cb1d7c306e11002c67a
Mozibix <opuzmoses@gmail.com>
2023-10-21 12:54:13 +0100

    chores: updated customer component

---
+++ src/pages/Customer.jsx
@@ -0,0 +1,11 @@
+import DashboardLayout from "../layouts/DashboardLayout";
+
+export const Customer = () => {
+  return (
+    <>
+      <DashboardLayout>
+        <p>hello world</p>
+      </DashboardLayout>
+    </>
+  );
+};
