1dac6df69b65ad9749a9b948640883213da5fb01
Mozibix <opuzmoses@gmail.com>
2023-10-22 06:42:15 +0100

    chores: updated customer page component

--- src/pages/Customer.jsx
+++ src/pages/Customer.jsx
@@ -1,10 +1,31 @@
+import { Reviews } from "../(components)/Reviews";
+import { TopCustomer } from "../(components)/TopCustomer";
+import { UsersComps } from "../(components)/UsersComps";
 import DashboardLayout from "../layouts/DashboardLayout";
 
 export const Customer = () => {
   return (
     <>
       <DashboardLayout>
-        <p>hello world</p>
+        <div className="customer">
+          <div className="customer_inner">
+            {/* USERS TABLE */}
+            <div className="customer_left">
+              <UsersComps />
+            </div>
+
+            <div className="customer_right">
+              {/* TOP CUSTOMER */}
+              <div>
+                <TopCustomer />
+              </div>
+              {/* TOP CUSTOMER */}
+              <div>
+                <Reviews />
+              </div>
+            </div>
+          </div>
+        </div>
       </DashboardLayout>
     </>
   );
