2f5677e0c9c0b967dee17cf8659b19ffdedad691
Mozibix <opuzmoses@gmail.com>
2023-10-22 05:49:53 +0100

    chores: updated revenue component

--- src/(components)/Revenue.jsx
+++ src/(components)/Revenue.jsx
@@ -1,3 +1,28 @@
+import { revenue_chart } from "../assets/image";
+
 export const Revenue = () => {
-  return <div>Revenue</div>;
+  return (
+    <>
+      <div className="revenue">
+        <div className="revenue_inner">
+          <h4>revenue</h4>
+
+          <img src={revenue_chart} alt="chart" />
+
+          <div className="revenue_bottom">
+            <ul>
+              <li>
+                <span className="colored_box first"></span>
+                2023
+              </li>
+              <li>
+                <span className="colored_box second"></span>
+                2022
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
