cb7bd9a8f124083a3af8780047b8427322f9a4a1
Mozibix <opuzmoses@gmail.com>
2023-11-27 23:43:10 +0100

    chores: updated settingsNav component

--- src/(components)/SettingsNav.jsx
+++ src/(components)/SettingsNav.jsx
@@ -1,14 +1,23 @@
-import { dashboard_primary_icon } from "../assets/image";
+import {
+  dashboard_dark_icon,
+  dashboard_primary_icon,
+  shopping_bag_icon,
+  shopping_bag_primary_icon,
+  subscription_dark_icon,
+  subscription_primary_icon,
+} from "../assets/image";
 
 const SettingsNav = () => {
   return (
     <aside className="settings_nav">
       <div className="settings_nav_inner">
         <ul>
-          <li>
-            <img src={dashboard_primary_icon} alt="nav-icon" />
-            account settings
-          </li>
+          {settingsItems.map((item) => (
+            <li key={item.id}>
+              <img src={item.icon_src} alt="nav-icon" />
+              {item.title}
+            </li>
+          ))}
         </ul>
       </div>
     </aside>
@@ -16,3 +25,29 @@ const SettingsNav = () => {
 };
 
 export default SettingsNav;
+
+const settingsItems = [
+  {
+    id: 1,
+    icon_src: dashboard_dark_icon,
+    icon_src_active: dashboard_primary_icon,
+    title: "account settings",
+    link: "/",
+  },
+
+  {
+    id: 2,
+    icon_src: shopping_bag_icon,
+    icon_src_active: shopping_bag_primary_icon,
+    title: "Security",
+    link: "/",
+  },
+
+  {
+    id: 3,
+    icon_src: subscription_dark_icon,
+    icon_src_active: subscription_primary_icon,
+    title: "Notification",
+    link: "/",
+  },
+];
