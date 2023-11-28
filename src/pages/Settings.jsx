8c29ee7513f1c945e081109e54e17a3664672c36
Mozibix <opuzmoses@gmail.com>
2023-11-28 13:22:21 +0100

    chores: updated settings component

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,18 +1,56 @@
-import SettingsNav from "../(components)/SettingsNav";
+import { user_round_icon } from "../assets/image";
 import DashboardLayout from "../layouts/DashboardLayout";
+import SettingsLayout from "../layouts/SettingsLayout";
 
 export const Settings = () => {
   return (
     <DashboardLayout>
-      <div className="settings_sec">
-        <div className="settings_inner">
-          <div>
-            <SettingsNav />
-          </div>
+      <SettingsLayout>
+        <div className="settings_sec">
+          <div className="settings_inner">
+            {/* SETTINGS HEADER */}
+            <div className="settings_header">
+              <div className="settings_header_inner">
+                <div className="settings_header_left">
+                  <h4>account settings</h4>
+                  <p>Adjust whichever setting you want here</p>
+                </div>
+                <div className="settings_header_right">
+                  <button className="hover_animation small"> cancel </button>
+                  <button className="hover_animation small">
+                    save changes
+                  </button>
+                </div>
+              </div>
+            </div>
 
-          <div>main</div>
+            {/* SETTINGS ACCT */}
+            <div className="settings_acct">
+              <div className="settings_acct_inner">
+                <h5>Profile Picture Upload</h5>
+
+                <div className="user_details">
+                  <div className="user_details_inner">
+                    <div className="user_details_left">
+                      <img src={user_round_icon} alt="user" />
+                      <div>
+                        <h6>Omosanyin Elijah Ayomide</h6>
+                        <span>admin</span>
+                      </div>
+                    </div>
+                    <div className="user_details_right">
+                      <button className="hover_animation small">
+                        upload new photo
+                      </button>
+                      <button className="hover_animation small">delete</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
+      </SettingsLayout>
     </DashboardLayout>
   );
 };
