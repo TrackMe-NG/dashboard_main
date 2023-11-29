e10d72686076434a04a97e4947c5d8d774b3cf15
Mozibix <opuzmoses@gmail.com>
2023-11-29 20:42:55 +0100

    chores: updated settingsLayout component

--- src/layouts/SettingsLayout.jsx
+++ src/layouts/SettingsLayout.jsx
@@ -1,7 +1,24 @@
-import PropTypes from "prop-types";
+import { useState } from "react";
 import SettingsNav from "../(components)/SettingsNav";
+import {
+  dashboard_dark_icon,
+  dashboard_primary_icon,
+  shopping_bag_icon,
+  shopping_bag_primary_icon,
+  subscription_dark_icon,
+  subscription_primary_icon,
+} from "../assets/image";
+import AccountSettings from "../(components)/(SettingsComps)/AccountSettings";
+import Security from "../(components)/(SettingsComps)/Security";
+import Notification from "../(components)/(SettingsComps)/Notification";
 
-const SettingsLayout = ({ children }) => {
+const SettingsLayout = () => {
+  const [selectedItem, setSelectedItem] = useState(settingsItems[0]);
+
+  const handleItemClick = (item) => {
+    setSelectedItem(item);
+  };
+
   return (
     <>
       <div className="settings_main">
@@ -9,13 +26,17 @@ const SettingsLayout = ({ children }) => {
           <div className="settings_main_left">
             {/* SIDENAV SECTION */}
             <div className="sidenav_main">
-              <SettingsNav />
+              <SettingsNav
+                settingsItems={settingsItems}
+                selectedItem={selectedItem}
+                onItemClick={handleItemClick}
+              />
             </div>
           </div>
           <div className="settings_main_right">
             <div className="settings_main_right_inner">
               {/* MAIN CONTENTS */}
-              <div className="settings_main_contents">{children}</div>
+              <div className="settings_main_contents">{selectedItem.comps}</div>
             </div>
           </div>
         </div>
@@ -26,6 +47,28 @@ const SettingsLayout = ({ children }) => {
 
 export default SettingsLayout;
 
-SettingsLayout.propTypes = {
-  children: PropTypes.node,
-};
+const settingsItems = [
+  {
+    id: 1,
+    icon_src: dashboard_dark_icon,
+    icon_src_active: dashboard_primary_icon,
+    title: "account settings",
+    comps: <AccountSettings />,
+  },
+
+  {
+    id: 2,
+    icon_src: shopping_bag_icon,
+    icon_src_active: shopping_bag_primary_icon,
+    title: "Security",
+    comps: <Security />,
+  },
+
+  {
+    id: 3,
+    icon_src: subscription_dark_icon,
+    icon_src_active: subscription_primary_icon,
+    title: "Notification",
+    comps: <Notification />,
+  },
+];
