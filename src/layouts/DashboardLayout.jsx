fffbbba9795aaf6fce9a8e8e17d8289c184e7952
Mozibix <opuzmoses@gmail.com>
2023-12-18 21:08:52 +0100

    chores: updated dashboardLayout component

--- src/layouts/DashboardLayout.jsx
+++ src/layouts/DashboardLayout.jsx
@@ -1,31 +1,64 @@
 import PropTypes from "prop-types";
 import { Header } from "../(components)/Header";
 import { SideNav } from "../(components)/SideNav";
+import { useEffect, useState } from "react";
 
 const DashboardLayout = ({ children }) => {
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setScreenWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <>
-      <div className="dashboard_main">
-        <div className="dashboard_main_inner">
-          <div className="dashboard_main_left">
-            {/* SIDENAV SECTION */}
-            <div className="sidenav_main">
-              <SideNav />
-            </div>
-          </div>
-          <div className="dashboard_main_right">
-            <div className="dashboard_main_right_inner">
-              {/* HEADER SECTION */}
-              <div>
-                <Header />
+      {screenWidth > 992 ? (
+        <>
+          <div className="dashboard_main">
+            <div className="dashboard_main_inner">
+              <div className="dashboard_main_left">
+                {/* SIDENAV SECTION */}
+                <div className="sidenav_main">
+                  <SideNav />
+                </div>
               </div>
+              <div className="dashboard_main_right">
+                <div className="dashboard_main_right_inner">
+                  {/* HEADER SECTION */}
+                  <div>
+                    <Header />
+                  </div>
 
-              {/* MAIN CONTENTS */}
-              <div className="dashboard_main_contents">{children}</div>
+                  {/* MAIN CONTENTS */}
+                  <div className="dashboard_main_contents">{children}</div>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-      </div>
+        </>
+      ) : (
+        <>
+          <div className="error_page_sec">
+            <div className="error_page_sec_inner">
+              <h2>opps!</h2>
+
+              <p className="quotes">
+                mobile-screen coming soon... <br />
+                <br />
+                screen-size &gt; 992px to view this application
+              </p>
+            </div>
+          </div>
+        </>
+      )}
     </>
   );
 };
